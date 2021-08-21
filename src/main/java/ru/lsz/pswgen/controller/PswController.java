8c81825d0e924a522eba13b651059ddf76f3f3c0
Rishat <spk_irbis@mail.ru>
2021-08-21 11:22:40 +0300

    Refactoring

--- src/main/java/ru/lsz/pswgen/controller/PswController.java
+++ src/main/java/ru/lsz/pswgen/controller/PswController.java
@@ -4,13 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ru.lsz.pswgen.service.PswService;
 
 import java.util.List;
 
 @Controller
-public class PswRestController {
+public class PswController {
     @Autowired
     PswService pswService;
 
